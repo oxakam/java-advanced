0e3fb1ac3513e140cbd4589477953fda2e54c3ba
oxakam <72361217+oxakam@users.noreply.github.com>
2020-11-24 19:08:13 +0100

    changed to testing 1

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello there... I am testing");
+		System.out.println("hello there... I am testing 1");
 
 	}
 
