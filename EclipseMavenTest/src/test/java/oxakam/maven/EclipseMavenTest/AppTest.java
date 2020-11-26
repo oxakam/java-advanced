610090339e88470d4ca05e4cc2d549bd1dd41d73
oxakam <72361217+oxakam@users.noreply.github.com>
2020-11-26 14:15:15 +0100

    Created

---
+++ EclipseMavenTest/src/test/java/oxakam/maven/EclipseMavenTest/AppTest.java
@@ -0,0 +1,20 @@
+package oxakam.maven.EclipseMavenTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
