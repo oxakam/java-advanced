309e1dcf7d09e6be39dd3367bdc6e1ca768c10aa
oxakam <72361217+oxakam@users.noreply.github.com>
2020-12-01 23:01:59 +0100

    changed in app.java

--- EclipseMavenTest/src/main/java/oxakam/maven/EclipseMavenTest/App.java
+++ EclipseMavenTest/src/main/java/oxakam/maven/EclipseMavenTest/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "I am here" );
+        System.out.println( "to test Maven" );
     }
 }
