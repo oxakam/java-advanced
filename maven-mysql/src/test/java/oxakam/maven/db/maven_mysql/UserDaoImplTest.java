f0d1696dba9b4b0f4ede128f9bdddf82a04b087a
oxakam <72361217+oxakam@users.noreply.github.com>
2020-12-08 19:31:43 +0100

    load text file, split stream into arrays of strings

--- maven-mysql/src/test/java/oxakam/maven/db/maven_mysql/UserDaoImplTest.java
+++ maven-mysql/src/test/java/oxakam/maven/db/maven_mysql/UserDaoImplTest.java
@@ -3,8 +3,14 @@ package oxakam.maven.db.maven_mysql;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,9 +19,43 @@ import org.junit.Test;
 public class UserDaoImplTest {
 	
 	private Connection conn;
+	private List<User> users;
+	private static final int NUM_TEST_USERS = 1000;
+	
+	private List<User> loadUsers() throws IOException {
+		
+		/** load text from a file and split into strings and turn those 
+		    into user objects using strings as user names 
+		
+		   return Files:
+			   .load lines of a file as a bunch of strings 
+			   .split lines in arrays of strings
+			   .bunch of lists [a,b,c,d]                         
+			   .unpack data structures within a list into words
+			   .filter words with length between 3 and 20 
+			   .turn words into items of User object
+			   .limit number of users 
+			   .add to the list of users  */
+		
+		// @formatter:off
+		
+		return Files
+			.lines(Paths.get("../greatexpectations.txt"))
+			.map(line -> line.split("[^A-Za-z]"))		
+			.map(Arrays::asList)
+			.flatMap(List -> List.stream())
+			.filter(word -> word.length() > 3 & word.length() < 20)
+			.map(word -> new User(word))
+			.limit(NUM_TEST_USERS)
+			.collect(Collectors.toList());
+		
+		// @formatter:on
+	}
 	
 	@Before
-	public void setUp() throws SQLException {
+	public void setUp() throws SQLException, IOException {
+		
+		users = loadUsers();
 		
 		var props = Profile.getProperties("db");
 		
