94454bfc1a43e71bff328d5944aecde0467c98a2
oxakam <72361217+oxakam@users.noreply.github.com>
2020-12-27 18:39:54 +0100

    created

---
+++ web-gradle/src/main/java/web/UserServlet.java
@@ -0,0 +1,48 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("doGet testing");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		doGet(request, response);
+		
+		var name = request.getParameter("nameName");
+		var password = request.getParameter("passName");
+		
+		response.setContentType("text/html");
+		response.getWriter().println("<html>Form submitted: " + name + "</html>");
+		
+		System.out.println(name + ": " + password);
+	}
+
+}
