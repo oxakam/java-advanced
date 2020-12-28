edb070c561957eec4529617263578d4c89788649
oxakam <72361217+oxakam@users.noreply.github.com>
2020-12-28 21:37:25 +0100

    spring database configuration

--- spring-boot/src/main/java/app/App.java
+++ spring-boot/src/main/java/app/App.java
@@ -2,8 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {"configuration", "main"})
+@EntityScan(basePackages = {"entities"})
 public class App {
 
 	public static void main(String[] args) {
