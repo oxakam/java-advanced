20dcca39381eba2f4dfda1921e5f3a1053605ba9
oxakam <72361217+oxakam@users.noreply.github.com>
2021-01-03 22:31:22 +0100

    Revert "creating database"
    This reverts commit 41b81798c4c7c1190bdf062ba64f438c878ee54b.

--- spring-web-service/src/main/java/app/entities/Book.java
+++ spring-web-service/src/main/java/app/entities/Book.java
@@ -1,24 +1,9 @@
 package app.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "books")
 public class Book {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(length = 100, nullable = false)
 	private String title;
-	
-	@Column(length = 100, nullable = false)
 	private String author;
 
 	public Book() {
