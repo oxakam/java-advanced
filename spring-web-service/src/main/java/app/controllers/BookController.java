1e349cdfb902fe76f72c7aad2920df6c63edb110
oxakam <72361217+oxakam@users.noreply.github.com>
2021-01-04 18:41:15 +0100

    implementing book controller

--- spring-web-service/src/main/java/app/controllers/BookController.java
+++ spring-web-service/src/main/java/app/controllers/BookController.java
@@ -1,10 +1,14 @@
 package app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Book;
@@ -17,33 +21,44 @@ public class BookController {
 	private BookService bookService;
 	
 	@GetMapping("/books")		//localhost:8080/books
-	public Book books() {
-		
-		var book = new Book("Charles Dickens", "Great Expectations");
-		
-		return book;
+	public List<Book> books() {
+	
+		return bookService.getAll();
 	}
 	
 	@PostMapping("/books")
-	public Book createBook(@RequestBody Book book) {
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void createBook(@RequestBody Book book) {
 		
-		System.out.println("Creating book: " + book);
-		
-		book.setId(7L);
-		
-		return book;
-	}	
-
+		bookService.save(book);
+	}
+	
 	@GetMapping("/books/{id}")
 	public Book getBook(@PathVariable("id") Long id) {
 		
-		var book = new Book("Charles Dickens", "Great Expectations");
-		
-		System.out.println("Book ID: " + id);
-		
-		book.setId(id);
-
-		return book;
+		return bookService.getBook(id);
 	}
+	
+//	@PostMapping("/books")
+//	public Book createBook(@RequestBody Book book) {
+//		
+//		System.out.println("Creating book: " + book);
+//		
+//		book.setId(7L);
+	
+//		return book;
+//	}	
+
+//	@GetMapping("/books/{id}")
+//	public Book getBook(@PathVariable("id") Long id) {
+//		
+//		var book = new Book("Charles Dickens", "Great Expectations");
+//		
+//		System.out.println("Book ID: " + id);
+//		
+//		book.setId(id);
+//
+//		return book;
+//	}
 	
 }
