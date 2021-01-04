4ed2c181e338bebcd8da4bc79fd0640d39338466
oxakam <72361217+oxakam@users.noreply.github.com>
2021-01-04 17:45:37 +0100

    posting JSON and path variables

--- spring-web-service/src/main/java/app/controllers/BookController.java
+++ spring-web-service/src/main/java/app/controllers/BookController.java
@@ -1,6 +1,9 @@
 package app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Book;
@@ -15,4 +18,27 @@ public class BookController {
 		
 		return book;
 	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		
+		System.out.println("Creating book: " + book);
+		
+		book.setId(7L);
+		
+		return book;
+	}	
+
+	@GetMapping("/books/{id}")
+	public Book getBook(@PathVariable("id") Long id) {
+		
+		var book = new Book("Charles Dickens", "Great Expectations");
+		
+		System.out.println("Book ID: " + id);
+		
+		book.setId(id);
+
+		return book;
+	}
+	
 }
